d55057a41496ff19171979663a705f340e0c8237
Tyuiu.TyazhovLA@outlook.com <99raindeath@gmail.com>
2024-11-23 15:55:15 +0500

    t5

--- Tyuiu.TyazhovLA.Sprint6.Task5.V4.Lib/DataService.cs
+++ Tyuiu.TyazhovLA.Sprint6.Task5.V4.Lib/DataService.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.TyazhovLA.Sprint6.Task5.V4.Lib
             
             for (int i = 0; i < valueArray.Length; i++)
             {
-                valuesArray[i]=Convert.ToDouble(valueArray[i]);
+                if (Convert.ToDouble(valueArray[i]) % 1 == 0) valuesArray[i] = Convert.ToDouble(valueArray[i]);
             }
             return valuesArray;
 
